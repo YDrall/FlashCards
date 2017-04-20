8839eb918b499e5f7eeb28294313c7f7fefe7947
ydrall <yogeshdrall7874@gmail.com>
2017-04-20 10:36:22 +0530

    create a database instance

--- app/src/main/java/ydrall/flashcards/FlashCardsApp.java
+++ app/src/main/java/ydrall/flashcards/FlashCardsApp.java
@@ -4,12 +4,16 @@ import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 
+import ydrall.flashcards.model.DataBase;
+
 public class FlashCardsApp extends Application {
 
+    public static DataBase dataBase;
     @Override
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(getApplicationContext());
+        dataBase = new DataBase(getApplicationContext());
     }
 
 }
